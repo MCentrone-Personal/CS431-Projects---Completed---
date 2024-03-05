a8d113192e831f710f55a909cc2211e8a45378d5
JoshBump <143889817+JoshBump@users.noreply.github.com>
2024-03-05 10:11:40 -0500

    Update luaHealing.lua

---
+++ Project2-LuaHealing/luaHealing.lua
@@ -0,0 +1,72 @@
+local mq = require('mq')
+local PackageMan = require('mq/PackageMan')
+local lfs = PackageMan.Require('luafilesystem', 'lfs')
+
+healingSpell = "Healing"
+buffSpell = "Holy Armor"
+
+function main()
+  tankName = ""
+  dpsName = ""
+
+  tankPos = 0
+  dpsPos = 0
+
+  if mq.Group < 2 then
+    print("You need at least 2 group members")
+    return
+  end
+
+  tankFound = false
+  dpsFound = false
+
+  for i = 1, mq.Group, +1 do
+    mq.cmd('/target ${Group.Member[%d].CleanName}', i)
+    s = mq.Target.Class()
+
+    if (s.Equal("Shadow Knight") or s.Equal("Paladin") or s.Equal("Warrior")) then
+      tankFound = true
+      tankPos = i
+    end
+
+    ---if s.Equal(<Name of DPS) then
+      ---dpsFound = true
+      ---dpsPos = i
+    ---end
+  end
+
+  if tankFound = true then
+    print("A tank was found.")
+  else
+    print("No tank found. Exiting...")
+    return
+  end
+
+  if dpsFound = true then
+    print("A DPS was found.")
+  ---else
+    ---print("No DPS found. Exiting...")
+    ---return
+  end
+
+  memSpells(healingSpell, buffSpell)
+
+  x = 0
+  while x == 0 do
+
+
+end
+
+function memSpells(healingSpell, buffSpell)
+  mq.cmd("/memspell 1 " .. healingSpell)
+  print("Delaying 14 seconds to memorize " .. healingSpell)
+  mq.cmd("/delay 14s")
+
+  mq.cmd("/memspell 2 " .. buffSpell)
+  print("Delaying 14 seconds to memorize " .. buffSpell)
+  mq.cmd("/delay 14s")
+
+  print("Support is ready")
+end
+
+main()
