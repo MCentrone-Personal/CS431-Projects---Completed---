a7f9d51e2b616a89241f51fa7e12e3df525c4a8d
SvenArcticFox <74692162+SvenArcticFox@users.noreply.github.com>
2024-04-04 10:06:02 -0400

    Add spawn and proximity say events

---
+++ Project3-NPC_Quest/npc.pl
@@ -0,0 +1,15 @@
+# Event when npc spawns
+sub EVENT_SPAWN {
+    #:: Create a proximity, 100 units across, 100 units tall, enable proximity say
+    quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 1);
+    #:: Also, enable proximity say
+    quest::enable_proximity_say();
+}
+
+# Message event for NPC, right now responds to hail
+sub EVENT_PROXIMITY_SAY {
+    #:: Match say message for "hail", /i for case insensitive
+    if ($text=~/hail/i) {
+        quest::say("Hello, $name!");
+    }
+}
