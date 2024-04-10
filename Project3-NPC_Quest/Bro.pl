15b6423d0639f3f20c06161050791e1141c60e52
CodingMan275 <mikeyc0723@gmail.com>
2024-04-09 22:20:24 -0400

    Add files via upload

--- Project3-NPC_Quest/Bro.pl
+++ Project3-NPC_Quest/Bro.pl
@@ -17,34 +17,33 @@ sub EVENT_SAY {
 		quest::say("Finally i can offer up a little [trade]");
 		quest::say("if you have nothing left to do [begone]");
 	}
-	elsif($text=~/buff/i)
+	if($text=~/buff/i)
 	{
 		$npc->CastSpell(@spell[0], $userid);
 	}
-	elsif($text=~/healing/i)
+	if($text=~/healing/i)
 	{
 		$npc->CastSpell(@spell[1], $userid);
 	}
-	elsif($text=~/begone/i)
+	if($text=~/begone/i)
 	{
 		$npc->CastSpell(2943, $userid);
-		quest::say("bye bye");
 	}
-	elsif($text=~/trade/i)
+	if($text=~/trade/i)
 	{
 		quest::say("Money has no monitary value to me, however if you give me 10,000p i will give you a level");
-		quest::say("you also have the option to give me 4,000p and  i will give you alternate advancement points");
+		quest::say("you also have the option to give me 4,000p and if you are level 52 or higher I will give you alternate advancement points");
 	}
-	elsif ($text=~/shrooms/i) 
+	if ($text=~/shrooms/i) 
 	{
 	quest::popup("Magic Shrooms", "I need the shrooms man. Give me that mushroom right there");
 	}
 
-	elsif($text=~/item/i)
+	if($text=~/item/i)
 	{
 	quest::say("In order for me to summon an item for you I need to know it's ID number. I will remember this item for when you want it");
 	}
-    elsif ($text=~/\d{0,9}/)
+    if ($text=~/\d{0,9}/)
 	{
 		$item = $text;
 	quest::say("You want item ID:$text");
@@ -70,10 +69,6 @@ sub EVENT_ITEM {
 		$client->AddAAPoints(1);
 		$total -= 4000;
 	}
-	elsif($ulevel < 52)
-	{
-		quest::say("You must be level 52 or higher to by AA");
-	}
 	
 	
 	#return any unused money
@@ -85,7 +80,6 @@ sub EVENT_ITEM {
 		$total = (($total - $silver_return)/10);
 		my $gold_return = $total % 10;
 		$total = (($total - $gold_return)/10);
-		quest::say("your change is $total$gold_return$silver_return$copper_return");
 		quest::givecash($copper_return,$silver_return,$gold_return,$total);
 	}
 
@@ -96,14 +90,12 @@ sub EVENT_ITEM {
 	quest::givecash(5,5,5,5);
     quest::ding();
 	quest::say("Thanks! This item can help you in the future!");
-    quest::updatetaskactivity(1448, 4);
   }
   
   elsif(plugin::check_handin(\%itemcount, 10037 => 1)) 
   {  
 	quest::say("Thanks! Now to give you item $item");
 	quest::summonitem("$item");
-    quest::updatetaskactivity(1448, 4);
   }  
   
   plugin::return_items(\%itemcount);
