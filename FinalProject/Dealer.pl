ba496297251da09ff322b5b53c7e278073a9ef7e
CodingMan275 <mikeyc0723@gmail.com>
2024-04-11 11:31:10 -0400

    Dealer.pl

--- FinalProject/Dealer.pl
+++ FinalProject/Dealer.pl
@@ -1,1 +1,30 @@
 
+# Message event for NPC, right now responds to hail
+sub EVENT_SAY {
+    #:: Match say message for "hail", /i for case insensitive
+    if ($text=~/hail/i)
+    {
+     quest::say("Hello, $name!. Give me your [money]");
+    }
+     if ($text=~/money/i)
+    {
+     quest::say("I like money");
+    }
+}
+
+sub EVENT_ITEM {
+	
+	my $total = ($platinum * 1000) + ($gold * 100) + ($silver * 10) + $copper;
+	#return any unused money
+	if($total > 0)
+	{
+	    my $copper_return = $total % 10;
+		$total = (($total - $copper_return)/10);
+		my $silver_return= $total % 10;
+		$total = (($total - $silver_return)/10);
+		my $gold_return = $total % 10;
+		$total = (($total - $gold_return)/10);
+		quest::givecash($copper_return,$silver_return,$gold_return,$total);
+	}
+  plugin::return_items(\%itemcount);
+}
