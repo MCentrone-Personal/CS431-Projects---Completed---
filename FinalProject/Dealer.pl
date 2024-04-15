413179187141260d73bea4ff7e3dbddadfa2982c
JoshBump <143889817+JoshBump@users.noreply.github.com>
2024-04-14 21:37:41 -0400

    Update Dealer.pl

--- FinalProject/Dealer.pl
+++ FinalProject/Dealer.pl
@@ -9,16 +9,25 @@ sub POKER_LOGIC {
 # Function call PokerLogic(int,int,int,int,int);
 #Will return a number, Highcard only will be a 1, Royal flush will be a 10
 
-$Card1 = $_[0];
-$Card2 = $_[1];
-$Card3 = $_[2];
-$Card4 = $_[3];
-$Card5 = $_[4];
+#Sort from Lowest to Highest, Card 1 lowest ... Card 5 Highest
+my @numeric = sort { $a <=> $b } ($_[0], $_[1], $_[2], $_[3], $_[4]);
+@CardCounter(0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+$Card1 = $numeric[0];
+$Card2 = $numeric[1];
+$Card3 = $numeric[2];
+$Card4 = $numeric[3];
+$Card5 = $numeric[4];
 
 $Flush = 0;
 $RoyalFlush = 0;
 $StraightFlush = 0;
-
+$FourKind = 0;
+$FullHouse = 0;
+$Straight = 0;
+$ThreeKind = 0;
+$TwoPair = 0;
+$OnePair = 0;
 $HighCard = 0;
 
 #Flush Check
@@ -45,12 +54,66 @@ if($Flush)
 	#Royal Flush
 	if((($Card1 % 13) + ($Card2 % 13) + ($Card3 % 13) + ($Card4 % 13) + ($Card5 % 13)) == 34)
 	{
-	$RoyalFlush = 2;
+	$RoyalFlush = 1;
+	}
+
+	#Straightflush
+	if((($Card1 + 1) == $Card2) and (($Card1 + 2) == $Card3) and (($Card1 + 3) == $Card4) and (($Card1 + 4) == $Card5))
+	{
+	$StraightFlush = 1;
 	}
 
 }
-	#Straightflush
-	if
+
+#Checking for Pairs, Triplets, Full House, ect.
+
+$CardCounter[$Card1 % 13]++;
+$CardCounter[$Card2 % 13]++;
+$CardCounter[$Card3 % 13]++;
+$CardCounter[$Card4 % 13]++;
+$CardCounter[$Card5 % 13]++;
+
+$PairsCounted = 0;
+
+for($i = 0; i <13; i++)
+{
+#Checking for 4 of a kind
+	if($CardCounter[i] == 4)
+	{
+	$FourKind = 1;
+	}
+
+#Checking for 3 of a kind
+	if(CardCounter[i] == 3)
+	{
+	$ThreeKind = 1;
+	}
+	
+#Checking for pairs
+	if(CardCounter[i] == 2)
+	{
+	$OnePair = 1;
+	$PairsCounted++;
+	}
+}
+
+#Full House check
+if($ThreeKind and $OnePair)
+{
+$FullHouse = 1;
+$ThreeKind = 0;
+$OnePair = 0;
+}
+
+#Two Pairs Check
+if($PairsCounted == 2)
+{
+$OnePair = 0;
+$TwoPair = 1;
+}
+
+#Final Return Checks
+
 
 }
 # Message event for NPC, right now responds to hail
