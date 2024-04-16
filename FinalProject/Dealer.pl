0cfc8cac5e0adf7dae6d1d601c8de76dbad3ae54
JoshBump <143889817+JoshBump@users.noreply.github.com>
2024-04-16 11:17:54 -0400

    Update Dealer.pl

--- FinalProject/Dealer.pl
+++ FinalProject/Dealer.pl
@@ -189,8 +189,7 @@ sub POKER_CARD_NAMES{
 	
 	@Names = ('Ace of Hearts', 'Two of Hearts', 'Three of Hearts', 'Four of Hearts', 'Five of Hearts', 'Six of Hearts', 'Seven of Hearts', 'Eight of Hearts', 'Nine of Hearts', 'Ten of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts','Ace of Clubs', 'Two of Clubs', 'Three of Clubs', 'Four of Clubs', 'Five of Clubs', 'Six of Clubs', 'Seven of Clubs', 'Eight of Clubs', 'Nine of Clubs', 'Ten of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs', 'Ace of Diamonds', 'Two of Diamonds', 'Three of Diamonds', 'Four of Diamondss', 'Five of Diamonds', 'Six of Diamondss', 'Seven of Diamonds', 'Eight of Diamonds', 'Nine of Diamonds', 'Ten of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds','Ace of Spades', 'Two of Spades', 'Three of Spades', 'Four of Spades', 'Five of Spades', 'Six of Spades', 'Seven of Spades', 'Eight of Spades', 'Nine of Spades', 'Ten of Spades', 'Jack of Spades', 'Queen of Spades', 'King of Spades');
 	
-	
-	
+	return($Names[$_[0] - 1]);
 	
 }
 # Message event for NPC, right now responds to hail
@@ -208,7 +207,7 @@ sub EVENT_SAY {
 	@Results = (POKER_LOGIC(int($Currenthand[0]),int($Currenthand[1]),$Currenthand[2],$Currenthand[3],$Currenthand[4]));
 	
     quest::say("I like money ");
-	quest::say($Results[0].' '.$Results[1].' '.$Results[2].' '.$Results[3].' '.$Results[4].' '.$Results[5]);
+	quest::say($Results[0].', '.POKER_CARD_NAMES($Results[1]).', '.POKER_CARD_NAMES($Results[2]).', '.POKER_CARD_NAMES($Results[3]).', '.POKER_CARD_NAMES($Results[4]).', '.POKER_CARD_NAMES($Results[5]));
     }
 }
 
