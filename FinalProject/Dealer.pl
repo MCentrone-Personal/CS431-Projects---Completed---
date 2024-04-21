4917f7b3735357bbc0efaf8877d0dea850f351e9
JoshBump <143889817+JoshBump@users.noreply.github.com>
2024-04-21 14:53:08 -0400

    Update Dealer.pl

--- FinalProject/Dealer.pl
+++ FinalProject/Dealer.pl
@@ -1,8 +1,29 @@
 
+our @Results = ();
+our @Currenthand = ();
+our @Newhand = ();
+our $Flag1 = 0;
+our $Flag2 = 0;
+our $Flag3 = 0;
+our $Flag4 = 0;
+our $Flag5 = 0;
+our $Card1F = "";
+our $Card2F = "";
+our $Card3F = "";
+our $Card4F = "";
+our $Card5F = "";
+our $UICard1 = "";
+our $UICard2 = "";
+our $UICard3 = "";
+our $UICard4 = "";
+our $UICard5 = "";
+
 sub POKER_START {
 	#Function to generate 5 random cards from one deck
 	@TempCards = (0,0,0,0,0);
 	@CurrentCards = (0,0,0,0,0);
+	if($_[0] != 1)
+	{
 	for($i = 0; $i<5; $i++)
 	{
 		TRYAGAIN:
@@ -14,6 +35,92 @@ sub POKER_START {
 		}
 		$CurrentCards[$i] = $TempCards[$i];
 	}
+	}
+	
+	if($_[0] == 1)
+	{
+		if($Flag1 == 1)
+		{
+			TRYAGAIN1:
+		$TempCards[0] = ((int rand(51)) + 1);
+		
+		if(($TempCards[0] == $_[1]) or ($TempCards[0] == $_[2]) or ($TempCards[0] == $_[3]) or ($TempCards[0] == $_[4]) or ($TempCards[0] == $_[5]))
+		{
+			goto TRYAGAIN1;
+		}
+		$CurrentCards[0] = $TempCards[0];
+		}
+		if($Flag1 == 0)
+		{
+		$CurrentCards[0] = $_[1];
+		}
+		
+		if($Flag2 == 1)
+		{
+			TRYAGAIN2:
+		$TempCards[1] = ((int rand(51)) + 1);
+		
+		if(($TempCards[1] == $_[1]) or ($TempCards[1] == $_[2]) or ($TempCards[1] == $_[3]) or ($TempCards[1] == $_[4]) or ($TempCards[1] == $_[5]))
+		{
+			goto TRYAGAIN2;
+		}
+		$CurrentCards[1] = $TempCards[1];
+		}
+		if($Flag2 == 0)
+		{
+		$CurrentCards[1] = $_[2];
+		}
+		
+		if($Flag3 == 1)
+		{
+			TRYAGAIN3:
+		$TempCards[2] = ((int rand(51)) + 1);
+		
+		if(($TempCards[2] == $_[1]) or ($TempCards[2] == $_[2]) or ($TempCards[2] == $_[3]) or ($TempCards[2] == $_[4]) or ($TempCards[2] == $_[5]))
+		{
+			goto TRYAGAIN3;
+		}
+		$CurrentCards[2] = $TempCards[2];
+		}
+		if($Flag3 == 0)
+		{
+		$CurrentCards[2] = $_[3];
+		}
+		
+		if($Flag4 == 1)
+		{
+			TRYAGAIN4:
+		$TempCards[3] = ((int rand(51)) + 1);
+		
+		if(($TempCards[3] == $_[1]) or ($TempCards[3] == $_[2]) or ($TempCards[3] == $_[3]) or ($TempCards[3] == $_[4]) or ($TempCards[3] == $_[5]))
+		{
+			goto TRYAGAIN4;
+		}
+		$CurrentCards[3] = $TempCards[3];
+		}
+		if($Flag4 == 0)
+		{
+		$CurrentCards[3] = $_[4];
+		}
+		
+		if($Flag5 == 1)
+		{
+			TRYAGAIN5:
+		$TempCards[4] = ((int rand(51)) + 1);
+		
+		if(($TempCards[4] == $_[1]) or ($TempCards[4] == $_[2]) or ($TempCards[4] == $_[3]) or ($TempCards[4] == $_[4]) or ($TempCards[4] == $_[5]))
+		{
+			goto TRYAGAIN5;
+		}
+		$CurrentCards[4] = $TempCards[4];
+		}
+		if($Flag5 == 0)
+		{
+		$CurrentCards[4] = $_[5];
+		}
+		
+	}
+	
   return ($CurrentCards[0],$CurrentCards[1],$CurrentCards[2],$CurrentCards[3],$CurrentCards[4]);
   
 }
@@ -144,50 +251,50 @@ $TwoPair = 1;
 #Final Return Checks
 if($RoyalFlush)
 {
-return(10,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return(10,$_[0], $_[1], $_[2], $_[3], $_[4], "Royal Flush");
 }
 elsif($StraightFlush)
 {
-return(9,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return(9,$_[0], $_[1], $_[2], $_[3], $_[4], "Straight Flush");
 }
 elsif($FourKind)
 {
-return(8,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return(8,$_[0], $_[1], $_[2], $_[3], $_[4], "Four of a Kind");
 }
 elsif($FullHouse)
 {
-return(7,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return(7,$_[0], $_[1], $_[2], $_[3], $_[4], "Full House");
 }
 elsif($Flush)
 {
-return(6,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return(6,$_[0], $_[1], $_[2], $_[3], $_[4], "Flush");
 }
 elsif($Straight)
 {
-return (5,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return (5,$_[0], $_[1], $_[2], $_[3], $_[4], "Straight");
 }
 elsif($ThreeKind)
 {
-return(4,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return(4,$_[0], $_[1], $_[2], $_[3], $_[4], "Three of a Kind");
 }
 elsif($TwoPair)
 {
-return(3,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return(3,$_[0], $_[1], $_[2], $_[3], $_[4], "Two Pair");
 }
 elsif($OnePair)
 {
-return (2,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return (2,$_[0], $_[1], $_[2], $_[3], $_[4], "Single Pair");
 }
 elsif($HighCard)
 {
-return (1,int($Card1),int($Card2),int($Card3),int($Card4),int($Card5));
+return (1,$_[0], $_[1], $_[2], $_[3], $_[4], "High Card");
 }
 
 }
 
 sub POKER_CARD_NAMES{
 	
-	@Names = ('Ace of Hearts', 'Two of Hearts', 'Three of Hearts', 'Four of Hearts', 'Five of Hearts', 'Six of Hearts', 'Seven of Hearts', 'Eight of Hearts', 'Nine of Hearts', 'Ten of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts','Ace of Clubs', 'Two of Clubs', 'Three of Clubs', 'Four of Clubs', 'Five of Clubs', 'Six of Clubs', 'Seven of Clubs', 'Eight of Clubs', 'Nine of Clubs', 'Ten of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs', 'Ace of Diamonds', 'Two of Diamonds', 'Three of Diamonds', 'Four of Diamondss', 'Five of Diamonds', 'Six of Diamondss', 'Seven of Diamonds', 'Eight of Diamonds', 'Nine of Diamonds', 'Ten of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds','Ace of Spades', 'Two of Spades', 'Three of Spades', 'Four of Spades', 'Five of Spades', 'Six of Spades', 'Seven of Spades', 'Eight of Spades', 'Nine of Spades', 'Ten of Spades', 'Jack of Spades', 'Queen of Spades', 'King of Spades');
+	@Names = ('Ace of Hearts', 'Two of Hearts', 'Three of Hearts', 'Four of Hearts', 'Five of Hearts', 'Six of Hearts', 'Seven of Hearts', 'Eight of Hearts', 'Nine of Hearts', 'Ten of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts','Ace of Clubs', 'Two of Clubs', 'Three of Clubs', 'Four of Clubs', 'Five of Clubs', 'Six of Clubs', 'Seven of Clubs', 'Eight of Clubs', 'Nine of Clubs', 'Ten of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs', 'Ace of Diamonds', 'Two of Diamonds', 'Three of Diamonds', 'Four of Diamonds', 'Five of Diamonds', 'Six of Diamondss', 'Seven of Diamonds', 'Eight of Diamonds', 'Nine of Diamonds', 'Ten of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds','Ace of Spades', 'Two of Spades', 'Three of Spades', 'Four of Spades', 'Five of Spades', 'Six of Spades', 'Seven of Spades', 'Eight of Spades', 'Nine of Spades', 'Ten of Spades', 'Jack of Spades', 'Queen of Spades', 'King of Spades');
 	
 	return($Names[$_[0] - 1]);
 	
@@ -195,27 +302,263 @@ sub POKER_CARD_NAMES{
 # Message event for NPC, right now responds to hail
 sub EVENT_SAY {
     #:: Match say message for "hail", /i for case insensitive
+	
     if ($text=~/hail/i)
     {
      quest::say("Hello, $name!. Give me your [money]");
+	 @Currenthand = (0,0,0,0,0);
+	 $Count = 0;
     }
      if ($text=~/money/i)
     {
 		
-	@Currenthand = (POKER_START());
+		
+	@Currenthand = (POKER_START(2,0,0,0,0,0));
 	
-	@Results = (POKER_LOGIC(int($Currenthand[0]),int($Currenthand[1]),$Currenthand[2],$Currenthand[3],$Currenthand[4]));
+	@Results = (POKER_LOGIC($Currenthand[0],$Currenthand[1],$Currenthand[2],$Currenthand[3],$Currenthand[4]));
 	
-	$Card1 = POKER_CARD_NAMES($Results[1]);
-	$Card2 = POKER_CARD_NAMES($Results[2]);
-	$Card3 = POKER_CARD_NAMES($Results[3]);
-	$Card4 = POKER_CARD_NAMES($Results[4]);
-	$Card5 = POKER_CARD_NAMES($Results[5]);
+	$Card1F = POKER_CARD_NAMES($Results[1]);
+	$Card2F = POKER_CARD_NAMES($Results[2]);
+	$Card3F = POKER_CARD_NAMES($Results[3]);
+	$Card4F = POKER_CARD_NAMES($Results[4]);
+	$Card5F = POKER_CARD_NAMES($Results[5]);
 	
     quest::say("I like money ");
-	quest::say($Results[0].', '.POKER_CARD_NAMES($Results[1]).', '.POKER_CARD_NAMES($Results[2]).', '.POKER_CARD_NAMES($Results[3]).', '.POKER_CARD_NAMES($Results[4]).', '.POKER_CARD_NAMES($Results[5]));
-	quest::say($Results[0].', '."[$Card5]".', '."[$Card1]".', '."[$Card2]".', '."[$Card3]".', '."[$Card4]");
+	quest::say($Results[0].', '."[$Card1F]".', '."[$Card2F]".', '."[$Card3F]".', '."[$Card4F]".', '."[$Card5F]");
+	
+	 my $intro = "Current Hand Power: ". $Results[0]. ". Hand Type: ". $Results[6];
+	 my $TextToCenter2 = plugin::PWAutoCenter($Card1F);
+	 my $TextToCenter3 = plugin::PWAutoCenter($Card2F);
+	 my $TextToCenter4 = plugin::PWAutoCenter($Card3F);
+	 my $TextToCenter5 = plugin::PWAutoCenter($Card4F);
+	 my $TextToCenter6 = plugin::PWAutoCenter($Card5F);
+
+	 my $Indent = plugin::PWIndent();
+	 my $Yel = plugin::PWColor("Yellow");
+	 my $Blu = plugin::PWColor("Light Blue");
+	 my $Red = plugin::PWColor("Red");
+	 my $grn = plugin::PWColor("Forest Green");
+	 
+	 $UICard1 = $Yel . $TextToCenter2;
+	 $UICard2 = $Yel . $TextToCenter3;
+	 $UICard3 = $Yel . $TextToCenter4;
+	 $UICard4 = $Yel . $TextToCenter5;
+	 $UICard5 = $Yel . $TextToCenter6;
+	 
+	  my $dialogMessage = "{title: Curernt Hand} {button_one: Confirm Hand} {button_two: Start Swapping} wintype:1 $intro </c> <br><br> $Yel $TextToCenter2 </c><br><br> $Yel $TextToCenter3 </c><br><br> $Yel $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+        quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count = 0;
+		$Flag1 = 0;
+		$Flag2 = 0;
+		$Flag3 = 0;
+		$Flag4 = 0;
+		$Flag5 = 0;
+	
     }
+	
+	if ($text=~/Start Swapping/i)
+	{
+		$Count = 0;
+		$Flag1 = 0;
+		$Flag2 = 0;
+		$Flag3 = 0;
+		$Flag4 = 0;
+		$Flag5 = 0;
+		
+		my $dialogMessage = "{title: $Card1F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $Red $TextToCenter2 </c><br><br> $Yel $TextToCenter3 </c><br><br> $Yel $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+	}
+	
+	if ($text=~/Swap/i)
+	{
+		my $intro = "Current Hand Power: ". $Results[0]. ". Hand Type: ". $Results[6];
+	 my $TextToCenter2 = plugin::PWAutoCenter($Card1F);
+	 my $TextToCenter3 = plugin::PWAutoCenter($Card2F);
+	 my $TextToCenter4 = plugin::PWAutoCenter($Card3F);
+	 my $TextToCenter5 = plugin::PWAutoCenter($Card4F);
+	 my $TextToCenter6 = plugin::PWAutoCenter($Card5F);
+
+	 my $Indent = plugin::PWIndent();
+	 my $Yel = plugin::PWColor("Yellow");
+	 my $Blu = plugin::PWColor("Light Blue");
+	 my $Red = plugin::PWColor("Red");
+	 my $grn = plugin::PWColor("Forest Green");
+	 
+		
+		if($Count == 0)
+		{
+			my $dialogMessage = "{title: $Card1F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $Red $TextToCenter2 </c><br><br> $Yel $TextToCenter3 </c><br><br> $Yel $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+		elsif($Count == 1)
+		{
+			$Flag1 = 1;
+			$UICard1 = $Blu . $TextToCenter2;
+			
+			my $dialogMessage = "{title: $Card2F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $Red $TextToCenter3 </c><br><br> $Yel $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+	elsif($Count == 2)
+		{
+			$Flag2 = 1;
+			$UICard2 = $Blu . $TextToCenter3;
+			
+			my $dialogMessage = "{title: $Card3F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $UICard2 </c><br><br> $Red $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+		elsif($Count == 3)
+		{
+			$Flag3 = 1;
+			$UICard3 = $Blu . $TextToCenter4;
+			my $dialogMessage = "{title: $Card4F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $UICard2 </c><br><br> $UICard3 </c><br><br> $Red $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+		elsif($Count == 4)
+		{
+			$Flag4 = 1;
+			$UICard4 = $Blu . $TextToCenter5;
+			my $dialogMessage = "{title: $Card5F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $UICard2 </c><br><br> $UICard3 </c><br><br> $UICard4 </c> <br><br> $Red $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+		elsif($Count == 5)
+		{
+			$Flag5 = 1;
+			$UICard5 = $Blu . $TextToCenter6;
+			my $dialogMessage = "{title: Final Hand} {button_one: Restart} {button_two: Confirm Hand} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $UICard2 </c><br><br> $UICard3 </c><br><br> $UICard4 </c> <br><br> $UICard5 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		
+		}
+	}
+	
+	if ($text=~/Restart/i)
+	{
+		$Count = 0;
+		$Flag1 =0;
+		$Flag2 =0;
+		$Flag3 =0;
+		$Flag4 =0;
+		$Flag5 =0;
+		
+		 my $intro = "Current Hand Power: ". $Results[0]. ". Hand Type: ". $Results[6];
+	 my $TextToCenter2 = plugin::PWAutoCenter($Card1F);
+	 my $TextToCenter3 = plugin::PWAutoCenter($Card2F);
+	 my $TextToCenter4 = plugin::PWAutoCenter($Card3F);
+	 my $TextToCenter5 = plugin::PWAutoCenter($Card4F);
+	 my $TextToCenter6 = plugin::PWAutoCenter($Card5F);
+
+	 my $Indent = plugin::PWIndent();
+	 my $Yel = plugin::PWColor("Yellow");
+	 my $Blu = plugin::PWColor("Light Blue");
+	 my $Red = plugin::PWColor("Red");
+	 my $grn = plugin::PWColor("Forest Green");
+	 
+	 $UICard1 = $Yel . $TextToCenter2;
+	 $UICard2 = $Yel . $TextToCenter3;
+	 $UICard3 = $Yel . $TextToCenter4;
+	 $UICard4 = $Yel . $TextToCenter5;
+	 $UICard5 = $Yel . $TextToCenter6;
+	 
+	  my $dialogMessage = "{title: Curernt Hand} {button_one: Confirm Hand} {button_two: Start Swapping} wintype:1 $intro </c> <br><br> $Yel $TextToCenter2 </c><br><br> $Yel $TextToCenter3 </c><br><br> $Yel $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+        quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		
+	}
+	
+	if ($text=~/Keep Card/i)
+	{
+		
+		my $intro = "Current Hand Power: ". $Results[0]. ". Hand Type: ". $Results[6];
+	 my $TextToCenter2 = plugin::PWAutoCenter($Card1F);
+	 my $TextToCenter3 = plugin::PWAutoCenter($Card2F);
+	 my $TextToCenter4 = plugin::PWAutoCenter($Card3F);
+	 my $TextToCenter5 = plugin::PWAutoCenter($Card4F);
+	 my $TextToCenter6 = plugin::PWAutoCenter($Card5F);
+
+	 my $Indent = plugin::PWIndent();
+	 my $Yel = plugin::PWColor("Yellow");
+	 my $Blu = plugin::PWColor("Light Blue");
+	 my $Red = plugin::PWColor("Red");
+	 my $grn = plugin::PWColor("Forest Green");
+	 
+		if($Count == 0)
+		{
+			$Flag1 = 0;
+			
+			my $dialogMessage = "{title: $Card1F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $Yel $TextToCenter2 </c><br><br> $Red $TextToCenter3 </c><br><br> $Yel $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+		elsif($Count == 1)
+		{
+			$Flag1 = 0;
+			my $dialogMessage = "{title: $Card2F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $Red $TextToCenter3 </c><br><br> $Yel $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+	elsif($Count == 2)
+		{
+			$Flag2 = 0;
+			my $dialogMessage = "{title: $Card3F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $UICard2 </c><br><br> $Red $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+		elsif($Count == 3)
+		{
+			$Flag3 = 0;
+			my $dialogMessage = "{title: $Card4F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $UICard2 </c><br><br> $UICard3</c><br><br> $Red $TextToCenter5 </c> <br><br> $Yel $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+		elsif($Count == 4)
+		{
+			$Flag4 = 0;
+			my $dialogMessage = "{title: $Card4F} {button_one: Keep Card} {button_two: Swap} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $UICard2 </c><br><br> $UICard3 </c><br><br> $UICard4 </c> <br><br> $Red $TextToCenter6 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		$Count++;
+		}
+		elsif($Count == 5)
+		{
+			$Flag5 = 0;
+			my $dialogMessage = "{title: Final Hand} {button_one: Restart} {button_two: Confirm Hand} wintype:1 $intro </c> <br><br> $UICard1 </c><br><br> $UICard2 </c><br><br> $UICard3 </c><br><br> $UICard4 </c> <br><br> $UICard5 </c> <br>";
+		quest::crosszonedialoguewindowbycharid($client->CharacterID(), $dialogMessage);
+		}
+	}
+	
+	
+	if ($text=~/Confirm Hand/i)
+     {
+		 
+    @Newhand = (POKER_START(1,$Currenthand[0],$Currenthand[1],$Currenthand[2],$Currenthand[3],$Currenthand[4]));
+	
+	@NewResults = (POKER_LOGIC($Newhand[0],$Newhand[1],$Newhand[2],$Newhand[3],$Newhand[4]));
+	
+	$Card1F = POKER_CARD_NAMES($NewResults[1]);
+	$Card2F = POKER_CARD_NAMES($NewResults[2]);
+	$Card3F = POKER_CARD_NAMES($NewResults[3]);
+	$Card4F = POKER_CARD_NAMES($NewResults[4]);
+	$Card5F = POKER_CARD_NAMES($NewResults[5]);
+	
+	my $intro = "Current Hand Power: ". $NewResults[0]. ". Hand Type: ". $NewResults[6];
+	 my $TextToCenter2 = plugin::PWAutoCenter($Card1F);
+	 my $TextToCenter3 = plugin::PWAutoCenter($Card2F);
+	 my $TextToCenter4 = plugin::PWAutoCenter($Card3F);
+	 my $TextToCenter5 = plugin::PWAutoCenter($Card4F);
+	 my $TextToCenter6 = plugin::PWAutoCenter($Card5F);
+
+	 my $Indent = plugin::PWIndent();
+	 my $Yel = plugin::PWColor("Yellow");
+	 my $Blu = plugin::PWColor("Light Blue");
+	 my $Red = plugin::PWColor("Red");
+	 my $grn = plugin::PWColor("Forest Green");
+	
+	 quest::popup("Results", "$intro </c> <br><br> $Yel $TextToCenter2 </c><br><br> $Yel $TextToCenter3 </c><br><br> $Yel $TextToCenter4 </c><br><br> $Yel $TextToCenter5 </c> <br><br> $Yel $TextToCenter6");
+	
+	quest::say($NewResults[0].', '."[$Card1F]".', '."[$Card2F]".', '."[$Card3F]".', '."[$Card4F]".', '."[$Card5F]");
+      }
 }
 
 sub EVENT_ITEM {
